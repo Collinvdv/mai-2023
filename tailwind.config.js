dbe4d38e1ec1e787e54454aac87138d970c81a75
Collin Van der Vorst <collin@littlemissrobot.com>
2023-05-31 14:45:14 +0200

    Add layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./templates/**/*.twig"
   ],
   theme: {
+    colors: {
+      "primary" : "#ACEAD5",
+      "shade" : "#2BB989",
+      "black" : "#272839",
+      "grey" : "#E9F3FD",
+      "white" : "#FFFFFF",
+      "sub1" : "#FFA592",
+      "sub2" : "#171DA1",
+      "sub3" : "#CEC4F7",
+      "sub4" : "#9D47A3",
+    },
     extend: {},
   },
   plugins: [],
